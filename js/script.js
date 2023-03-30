3edd2c418caf5e257c2b29d8cafde9523a154f34
lucaianl <lucaianml@gmail.com>
2023-03-30 19:57:49 -0300

    menu hamburguer e responsividade na galera e home

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const modalTriggers = document.querySelectorAll('.modal-trigger');
 const modal = document.querySelector('.modal');
 const closeModalButton = modal.querySelector('.buttons-modal-cancel');
 
+
+
 modalTriggers.forEach(modalTrigger => {
     modalTrigger.addEventListener('click', () => {
         modal.style.display = 'block';
@@ -11,3 +13,14 @@ modalTriggers.forEach(modalTrigger => {
 closeModalButton.addEventListener('click', () => {
     modal.style.display = 'none';
 });
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/assets/hamburgMenu.svg"
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "/assets/closeMenu.svg"
+    }
+}
