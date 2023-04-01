a0d9590714a2bfffd4d59ad6ac40575476495eb9
lucaianl <lucaianml@gmail.com>
2023-04-01 14:55:17 -0300

    responsividade em vender, freelancer e em models

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-
 const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-        // remove a classe 'active' de todos os links
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-        });
-        // adiciona a classe 'active' ao link que foi clicado
-        this.classList.add('active');
-
-    });
+  if (link.href === window.location.href) {
+    link.classList.add('active');
+  }
 });
-
 
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
